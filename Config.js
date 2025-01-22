4f8315bebfb1f1d0442ab68a58322f5608adec68
osho2025 <bellodivine135@gmail.com>
2025-01-22 13:40:59 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,19 +1,19 @@
 // hey there, kindly read this file till the end for better configuration
 
 global.settings = {
-  OWNER_NUMBERS: "2348105978475", // your number with country code
-  OWNER_NAME: 'OSHO', // your owner name 
+  OWNER_NUMBERS: "2349037241013", // your number with country code
+  OWNER_NAME: 'FURY', // your owner name 
   BOT_NAME: "DARKWEB/NET", // your preferred bot name
-  SESSION_ID: 'kord_ai-rePkbcIyTlkb', // replace your_sessionid with yours and make sure its inside rhe quotes
+  SESSION_ID: 'kord_ai-0IgRsubtkCQR', // replace your_sessionid with yours and make sure its inside rhe quotes
   INVINCIBLE_MODE: false, // toogle invincible mode 
   WORK_MODE: "private",
   TIME_ZONE: "Africa/Lagos",
   ANTI_VIEWONCE: true,
   READ_ALL_MESSAGES: false,
   AUTO_READ_STATUS: true,
-  PREFIX: ["/"],
+  PREFIX: ["."],
   GROUP_EVENTS: true,
-  WELCOME_MESSAGE: "Hello! I'm OSHO-AI, your friendly WhatsApp bot.",
+  WELCOME_MESSAGE: "Hello! I'm Fury-AI, your friendly WhatsApp bot.",
   DEFAULT_TRANSLATION_LANG: "en",
   COMMAND_COOLDOWN_TIME_IN_MS: 0.01,
   MAX_COMMANDS_PER_MINUTE: 100,
@@ -21,9 +21,9 @@ global.settings = {
   AUTO_LIKE_STATUS: false,
   AUTO_LIKE_EMOJI: '🐦‍⬛',
   BANKING_DETAILS: {
-    bank: "UBA", // the name of your preferred bank
-    acc_number: "2340267307", // account number for your preferred bank
-    acc_name: "DIVINE ESHIKOGIE BELLO" // name of your bank
+    bank: "Opay", // the name of your preferred bank
+    acc_number: "9037241013", // account number for your preferred bank
+    acc_name: "Dike Willington Chibuchi" // name of your bank
   },
   /* You can also replace the numbers with your addresses if you're using crypto, a command for this will come soon. */
   API_KEYS: {
