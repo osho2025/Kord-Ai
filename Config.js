f85d4278a39a3875c86d11dc1e3a546aa12fbced
osho2025 <bellodivine135@gmail.com>
2025-01-22 11:22:44 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,29 +1,29 @@
 // hey there, kindly read this file till the end for better configuration
 
 global.settings = {
-  OWNER_NUMBERS: "2349067339193", // your number with country code
-  OWNER_NAME: 'Miracle', // your owner name 
-  BOT_NAME: "KORD", // your preferred bot name
-  SESSION_ID: 'your_sessionid', // replace your_sessionid with yours and make sure its inside rhe quotes
+  OWNER_NUMBERS: "2348105978475", // your number with country code
+  OWNER_NAME: 'OSHO', // your owner name 
+  BOT_NAME: "DARKWEB/NET", // your preferred bot name
+  SESSION_ID: 'kord_ai-VusIStGIgeqW', // replace your_sessionid with yours and make sure its inside rhe quotes
   INVINCIBLE_MODE: false, // toogle invincible mode 
-  WORK_MODE: "Public",
+  WORK_MODE: "private",
   TIME_ZONE: "Africa/Lagos",
   ANTI_VIEWONCE: true,
   READ_ALL_MESSAGES: false,
   AUTO_READ_STATUS: true,
-  PREFIX: [".", "/", "!"],
+  PREFIX: ["/"],
   GROUP_EVENTS: true,
-  WELCOME_MESSAGE: "Hello! I'm KORD-AI, your friendly WhatsApp bot.",
+  WELCOME_MESSAGE: "Hello! I'm OSHO-AI, your friendly WhatsApp bot.",
   DEFAULT_TRANSLATION_LANG: "en",
   COMMAND_COOLDOWN_TIME_IN_MS: 0.01,
   MAX_COMMANDS_PER_MINUTE: 100,
   MAX_DOWNLOAD_SIZE: 100,
   AUTO_LIKE_STATUS: true,
-  AUTO_LIKE_EMOJI: '🍁',
+  AUTO_LIKE_EMOJI: '🐦‍⬛',
   BANKING_DETAILS: {
-    bank: "Kord-Ai Bank", // the name of your preferred bank
-    acc_number: "1234567890", // account number for your preferred bank
-    acc_name: "MyAccountName" // name of your bank
+    bank: "UBA", // the name of your preferred bank
+    acc_number: "2340267307", // account number for your preferred bank
+    acc_name: "DIVINE ESHIKOGIE BELLO" // name of your bank
   },
   /* You can also replace the numbers with your addresses if you're using crypto, a command for this will come soon. */
   API_KEYS: {
@@ -31,11 +31,11 @@ global.settings = {
   },
   isHackEnable: true,
   SAFE_SEARCH: true,
-  REJECT_CALLS: true,
+  REJECT_CALLS: false,
   SEND_WELCOME_MESSAGE: false,
   WELCOME_MESSAGE: "👋 Welcome to the group! We're glad to have you here. 😊",
   DELETE_WA_LINKS: false,
   BAD_WORD_FILTER: true,
-  BAD_WORDS: ['badword1', 'badword2', 'fuck', 'bitch', 'hentai'],
+  BAD_WORDS: ['badword1', 'pussy', 'fuck', 'bitch', 'hentai'],
   ANTI_DELETE_ENABLED: true,
 };
